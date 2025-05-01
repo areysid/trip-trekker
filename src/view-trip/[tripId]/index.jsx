cefea6aaa67c0e5fac73177583ef4dd5c1305eca
areysid <sidharthmalpani@gmail.com>
2025-05-01 10:01:22 +0530

    Fix import path case for Footer component

--- src/view-trip/[tripId]/index.jsx
+++ src/view-trip/[tripId]/index.jsx
@@ -6,7 +6,7 @@ import { toast } from 'sonner';
 import InfoSection from './components/InfoSection';
 import Hotels from './components/Hotels';
 import PlacesToVisit from './components/PlacesToVisit';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 
 function Viewtrip() {
     const {tripId}=useParams();
